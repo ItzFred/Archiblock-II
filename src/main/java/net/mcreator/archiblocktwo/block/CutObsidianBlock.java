689300c0bf3778e06cb55113c3021354b04b9874
ItzFred <fredl@DESKTOP-840NJBC>
2022-02-08 18:14:11 +0100

    Hope this works

--- src/main/java/net/mcreator/archiblocktwo/block/CutObsidianBlock.java
+++ src/main/java/net/mcreator/archiblocktwo/block/CutObsidianBlock.java
@@ -1,15 +1,25 @@
 
 package net.mcreator.archiblocktwo.block;
 
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.material.MaterialColor;
 import net.minecraft.world.level.material.Material;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.TieredItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.core.BlockPos;
+
+import java.util.List;
+import java.util.Collections;
 
 public class CutObsidianBlock extends Block {
-
 	public CutObsidianBlock() {
 		super(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.STONE).strength(50f, 10f).requiresCorrectToolForDrops());
-
 		setRegistryName("cut_obsidian");
 	}
 
@@ -32,11 +42,9 @@ public class CutObsidianBlock extends Block {
 
 	@Override
 	public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 		if (!dropsOriginal.isEmpty())
 			return dropsOriginal;
 		return Collections.singletonList(new ItemStack(this, 1));
 	}
-
 }
