4666c4ddf7ee9a3a3bf17a478b13d7aeabdfeb9b
ItzFred <fredl@DESKTOP-840NJBC>
2022-02-13 19:47:32 +0100

    Something weird

--- src/main/java/net/mcreator/archiblocktwo/block/TwilightBrickSlabBlock.java
+++ src/main/java/net/mcreator/archiblocktwo/block/TwilightBrickSlabBlock.java
@@ -1,15 +1,26 @@
 
 package net.mcreator.archiblocktwo.block;
 
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.material.MaterialColor;
 import net.minecraft.world.level.material.Material;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import net.minecraft.world.level.block.state.properties.SlabType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.TieredItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.core.BlockPos;
+
+import java.util.List;
+import java.util.Collections;
 
 public class TwilightBrickSlabBlock extends SlabBlock {
-
 	public TwilightBrickSlabBlock() {
 		super(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.STONE).strength(4f, 9f).requiresCorrectToolForDrops());
-
 		setRegistryName("twilight_brick_slab");
 	}
 
@@ -32,5 +43,4 @@ public class TwilightBrickSlabBlock extends SlabBlock {
 			return dropsOriginal;
 		return Collections.singletonList(new ItemStack(this, state.getValue(TYPE) == SlabType.DOUBLE ? 2 : 1));
 	}
-
 }
